efd8ab2a66aa9b6e7d6653e000181b297bbdaa44
eddieduro <e.jamesduro@gmail.com>
2016-02-11 16:15:40 -0800

    add pseudo code for card class

--- src/Card.php
+++ src/Card.php
@@ -1,6 +1,24 @@
 <?php
   class Card
   {
+    private $score;
+    private $description;
+
 
   }
 ?>
+card class
+->score
+->desc
+
+$cardOne = new Card(rand(1,10), firstcard);
+$cardOne->save();
+
+$allCards = array($cardOne....)
+
+$allCards[0];
+
+draw()
+if (card.getDesc == firstcard) {
+  card.drawNewCard()
+}
